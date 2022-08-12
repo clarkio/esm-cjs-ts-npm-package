7ac9eceaa7ed45eedb8c7beda6f9586effb56925
Brian Clark <clarkio@users.noreply.github.com>
2022-08-12 20:10:05 +0000

    fix: IT WAS A FREAKING TYPO! 😡

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-export default function helloWordTs() {
+export function helloWorldTs() {
   const message =
     'Hello World from a TypeScript npm package that supports both the CommonJS and ECMAScript module formats';
   console.log(message);
